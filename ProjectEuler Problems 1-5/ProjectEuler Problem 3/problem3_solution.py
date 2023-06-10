4a0b13f6342a8830aac252a5b9cce6453726118b
jmessina <j.w.messina24@gmail.com>
2023-06-10 03:50:14 -0700

    Finished Problem 3; had most of the algorithm planned out, but I didn't account for the smaller divisor to be a potential prime until seeing the results return an obviously wrong answer of 1

--- ProjectEuler Problems 1-5/ProjectEuler Problem 3/problem3_solution.py
+++ ProjectEuler Problems 1-5/ProjectEuler Problem 3/problem3_solution.py
@@ -7,6 +7,29 @@
 #       - approach 2: find and use some already built method for finding if a number is prime
 # 2) Use that function to find largest prime factor
 #       - approach 1: increments starting at 3, if value divides evenly (check using modulus operator)
-#         then check if its factor pair partner is prime --> first that does is returned
+#         then check if its factor pair partner is prime --> first success is returned
 #       - approach 2: decrements from product/3 rounded down, checks if number is prime
 #         --> if so, return it
+def isPrime(num):
+    for x in range(2, int(num/3)):
+        if num % x == 0:
+            return False
+    return True
+
+
+def findHighestPrimeFactor(inputValue):
+    largestPrimeFactorFound = 1
+    for x in range(2, int(inputValue/3)):
+        if inputValue % x == 0:
+            if isPrime(inputValue/x):
+                return inputValue/x
+            elif isPrime(x):
+                largestPrimeFactorFound = x
+        if x >= int(inputValue/x):
+            #print("Approx root reached at", inputValue/x)
+            return largestPrimeFactorFound
+    # normally, the function should never reach the next line, so it will have a distinct return value just in case
+    return 0
+
+pe_inputValue = 600851475143
+print("The largest prime factor of", pe_inputValue, "is", findHighestPrimeFactor(pe_inputValue))
