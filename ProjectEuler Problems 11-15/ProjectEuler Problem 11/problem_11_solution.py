76ed00abd03d3d1a6d4f469565311c2da02f217a
jmessina <j.w.messina24@gmail.com>
2023-10-17 17:56:32 -0700

    Finished implementation; was practically hallucinating due to sleep deprivation, so it took a really long time to mentally picture the 4 for-loops iterating through the grid in different ways

--- ProjectEuler Problems 11-15/ProjectEuler Problem 11/problem_11_solution.py
+++ ProjectEuler Problems 11-15/ProjectEuler Problem 11/problem_11_solution.py
@@ -4,16 +4,62 @@ with open("problem_11_20x20_grid.txt", "r") as theGRID:  # open file reading of 
     grid_as_2D_list = []
     temp_list = []
 
+    #
     for line in theGRID.readlines():
         temp_list = []
+        temp_str = ''
         for char in line:
             if char >= '0' and char <= '9':
                 temp_str += char
                 if len(temp_str) >= 2:
                     temp_list.append(int(temp_str))
-                    temp_str = ""
+                    temp_str = ''
             else:
                 continue
         grid_as_2D_list.append(temp_list)
 
+    # grid_as_2D_list should now be a list of list of ints
 
+    # let's first process the horizontal direction
+    greatest_product = 0
+    num_of_multiplicands = 4
+    for line in grid_as_2D_list:
+        for item_index in range(0, len(line) - num_of_multiplicands):
+            temp_product = 1
+            for offset in range(0,4):
+                temp_product *= line[item_index + offset]
+            # by this line we have the product of the numbers in the current grouping
+            if greatest_product < temp_product:
+                greatest_product = temp_product
+    print("Greatest adjacent product of 4 numbers in the grid is:", greatest_product)
+
+
+    # now vertical direction
+    for row in range(0, len(grid_as_2D_list) - num_of_multiplicands):
+        for col in range(0, len(grid_as_2D_list[row])):
+            temp_product = 1
+            for offset in range(0,4):
+                temp_product *= grid_as_2D_list[row + offset][col]
+            if greatest_product < temp_product:
+                greatest_product = temp_product
+    print("Greatest adjacent product of 4 numbers in the grid is:", greatest_product)
+
+    # now bottom-left to upper-right diagonal
+    for row in range(num_of_multiplicands - 1, len(grid_as_2D_list)):
+        for col in range(0, len(grid_as_2D_list[row]) - num_of_multiplicands):
+            temp_product = 1
+            for offset in range(0, 4):
+                temp_product *= grid_as_2D_list[row - offset][col + offset]
+            if greatest_product < temp_product:
+                greatest_product = temp_product
+    print("Greatest adjacent product of 4 numbers in the grid is:", greatest_product)
+
+    # now upper-left to bottom right diagonal
+    for row in range(0, len(grid_as_2D_list) - num_of_multiplicands):
+        for col in range(0, len(grid_as_2D_list[row]) - num_of_multiplicands):
+            temp_product = 1
+            for offset in range(0, 4):
+                temp_product *= grid_as_2D_list[row + offset][col + offset]
+            if greatest_product < temp_product:
+                greatest_product = temp_product
+    print("Greatest adjacent product of 4 numbers in the grid is:", greatest_product)
