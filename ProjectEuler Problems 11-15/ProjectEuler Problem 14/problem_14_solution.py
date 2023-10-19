b1b90dbf6eba883a57d4bd9f79c1c95331b771fa
jmessina <j.w.messina24@gmail.com>
2023-10-19 13:41:18 -0700

    Finished Problem 14; side note - I watched a coding video beforehand of someone writing a ReactNative social app; they just wrote code on screen elegantly and efficiently --> I think channeled some of its energy to wind up coding this rather quickly, because I solved it in about 10 minutes or less

--- ProjectEuler Problems 11-15/ProjectEuler Problem 14/problem_14_solution.py
+++ ProjectEuler Problems 11-15/ProjectEuler Problem 14/problem_14_solution.py
@@ -1,13 +1,24 @@
-import math
-from Algorithms.find_number_of_divisors import *
+def Collatzify(num):
+    if num % 2 == 0:
+        return num/2
+    else:
+        return (3*num) + 1
 
-target_num_of_divisors = 500
-found = False
-triangle_number = 55
-adder = 11
+best_num = 1
+best_Collatz_chain = 1
 
-while find_number_of_divisors(triangle_number) < 500:
-    triangle_number += adder
-    adder+=1
+for x in range(1, 1000000):
+    result = x
+    result_Collatz_chain = 1
+    while result != 1:
+        result = Collatzify(result)
+        result_Collatz_chain += 1
+    if result_Collatz_chain > best_Collatz_chain:
+        best_num = x
+        best_Collatz_chain = result_Collatz_chain
+        print("Best known seed updated:", best_num)
+        print("Chain Length:", best_Collatz_chain)
+        print()
 
-print("The first triangle number with at least 500 divisors is", triangle_number)
+print("The best Collatz chain seed under 1M is", best_num)
+print("with a chain length of", best_Collatz_chain)
