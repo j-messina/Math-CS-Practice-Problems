44484a29a9d8a23f2f74b41ab2212896cb9059f6
jmessina <j.w.messina24@gmail.com>
2023-12-20 11:06:04 -0800

    Solved Problem 25

--- ProjectEuler Problems 21-25/ProjectEuler Problem 25/problem_25_solution.py
+++ ProjectEuler Problems 21-25/ProjectEuler Problem 25/problem_25_solution.py
@@ -2,3 +2,25 @@ import sys
 sys.path.append('/Users/johnmessina/Documents/GitHub/Math-CS-Practice-Problems/Math-CS-Practice-Problems/')
 from Algorithms.RemovePrintPath import *
 RemovePrintPath()
+
+# What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+# Let's start with viewing the fibonacci sequence up to a certain point
+def Fibonacci_Numbers_By_DigitsCount(digit_count):
+    fib_list = [1,1]
+
+    while len(str(fib_list[-1])) < 1000:
+        fib_list.append(fib_list[-1]+fib_list[-2])
+    return fib_list
+
+results = Fibonacci_Numbers_By_DigitsCount(1000)
+print(len(results))
+
+# 1 digit  = 6
+# 2 digits = 5
+# 3 digits = 5
+# 4 digits = 4
+# 5 digits = 5
+# 6 digits = 5
+# 7 digits = 5
+# 8 digits = 4
