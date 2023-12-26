12293a1f73384c80c414b8fef3b64619f3c6d8d9
jmessina <j.w.messina24@gmail.com>
2023-12-26 11:17:30 -0800

    Finished Problem 30 and started new folder for problems 31-35

--- ProjectEuler Problems 26-30/ProjectEuler Problem 30/problem_30_solution.py
+++ ProjectEuler Problems 26-30/ProjectEuler Problem 30/problem_30_solution.py
@@ -2,3 +2,22 @@ import sys
 sys.path.append('/Users/johnmessina/Documents/GitHub/Math-CS-Practice-Problems/Math-CS-Practice-Problems/')
 from Algorithms.RemovePrintPath import *
 RemovePrintPath()
+
+def is_SO5PD(num):
+    num_as_str = str(num)
+    mysum = 0
+    for digit in num_as_str:
+        digit = int(digit)
+        mysum += digit**5
+    if num == mysum:
+        return True
+    else:
+        return False
+
+sum_results = 0
+for x in range(2,1000000):
+    if is_SO5PD(x):
+        print("Found valid number:", x)
+        sum_results+=x
+
+print("Sum of them all:", sum_results)
---
+++ ProjectEuler Problems Folder Template copy/ProjectEuler Problem _ 1/README.md
@@ -0,0 +1,5 @@
+<h1> ProjectEuler Problem _: </h1>
+
+<hr>
+
+URL: https://projecteuler.net/problem=<NUMBER_HERE>
