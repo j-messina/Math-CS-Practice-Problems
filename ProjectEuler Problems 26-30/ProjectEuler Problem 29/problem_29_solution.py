5ff00551cda84d4304af2d728ebd149e3e40ff81
jmessina <j.w.messina24@gmail.com>
2023-12-26 11:00:18 -0800

    Solved Problem 29

--- ProjectEuler Problems 26-30/ProjectEuler Problem 29/problem_29_solution.py
+++ ProjectEuler Problems 26-30/ProjectEuler Problem 29/problem_29_solution.py
@@ -2,3 +2,10 @@ import sys
 sys.path.append('/Users/johnmessina/Documents/GitHub/Math-CS-Practice-Problems/Math-CS-Practice-Problems/')
 from Algorithms.RemovePrintPath import *
 RemovePrintPath()
+
+myset = set()
+for a in range(2, 101):
+    for b in range(2, 101):
+        myset.add(a**b)
+
+print(len(myset))
