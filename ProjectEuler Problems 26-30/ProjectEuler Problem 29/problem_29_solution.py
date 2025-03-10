c64a3711dd38db2e226cd30119332b26579aec54
jmessina <j.w.messina24@gmail.com>
2025-03-10 16:49:44 -0600

    Fiddled with the stupid debugger, updated the path text for loading from the Algorithms folder

--- ProjectEuler Problems 26-30/ProjectEuler Problem 29/problem_29_solution.py
+++ ProjectEuler Problems 26-30/ProjectEuler Problem 29/problem_29_solution.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/Users/johnmessina/Documents/GitHub/Math-CS-Practice-Problems/Math-CS-Practice-Problems/')
+sys.path.append('C:/Users/jwmes/Documents/GitHub/Math-CS-Practice-Problems')
 from Algorithms.RemovePrintPath import *
 RemovePrintPath()
 
