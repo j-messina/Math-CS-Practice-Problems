25abb64a1845dc8b59ebd7ca05807c30876eb8ad
jmessina <j.w.messina24@gmail.com>
2025-03-10 17:56:48 -0600

    Another Failed Attempt
    tried to implement algorithm that goes from largest coins downwards; seems to have overcounted the total possible combinations, not sure why; thinking of using sets and checking for unique combos next

--- ProjectEuler Problems 31-35/ProjectEuler Problem 31/problem_31_solution.py
+++ ProjectEuler Problems 31-35/ProjectEuler Problem 31/problem_31_solution.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/Users/johnmessina/Documents/GitHub/Math-CS-Practice-Problems/Math-CS-Practice-Problems/')
+sys.path.append('C:/Users/jwmes/Documents/GitHub/Math-CS-Practice-Problems')
 from Algorithms.RemovePrintPath import *
 RemovePrintPath()
 
@@ -11,3 +11,67 @@ for x in list_of_vals_in_pence:
         list_desc[-1].append(y)
 # print(list_desc)
         
+
+"""
+starting val: 200, then 100, then 50, then 10, then 5, then 2, 1
+
+branches like a tree - can try self or lower value - always self first, then next highest value below self
+
+keep a running sum of all the coins
+keep a running counter of all found combinations
+on arriving at 200, no more recursion, increment counter by one
+on still below 200, recursive call for each coin option still available
+have a starting list of all coins
+having a running list of remaining available coins
+update list as available coins shrinks
+"""
+
+all_coins = [200,100,50,20,10,5,2,1]
+
+simple_version = [10,5,2,1]
+simple_max = 10
+true_max = 200
+
+mycounter = 0
+
+import copy
+
+def simpleCoins(count = simple_max, remaining_coins = copy.deepcopy(simple_version)):
+    global mycounter
+    for x in remaining_coins:
+        if x + count == simple_max:
+            mycounter += 1
+            templist = copy.deepcopy(remaining_coins)
+            templist.pop(0)
+        elif x + count > simple_max:
+            templist = copy.deepcopy(remaining_coins)
+            templist.pop(0)
+        else: # x + count < simple_max
+            templist = copy.deepcopy(remaining_coins)
+            count += x
+
+        if len(templist) > 0:
+            simpleCoins(count, templist)
+
+
+# simpleCoins(0)
+
+def countPence(count = true_max, remaining_coins = copy.deepcopy(all_coins)):
+    global mycounter
+    for x in remaining_coins:
+        if x + count == simple_max:
+            mycounter += 1
+            templist = copy.deepcopy(remaining_coins)
+            templist.pop(0)
+        elif x + count > simple_max:
+            templist = copy.deepcopy(remaining_coins)
+            templist.pop(0)
+        else: # x + count < true_max
+            templist = copy.deepcopy(remaining_coins)
+            count += x
+        if len(templist) > 0:
+            countPence(count, templist)
+
+countPence(0)
+
+print("Results are: ", mycounter)
